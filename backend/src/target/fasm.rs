8627335b9bc17871cdaebba105510aa7ea55bed5
Chris Liourtas <krackedissad@gmail.com>
2024-08-11 09:16:47 +0000

    Started fasm implementation

--- backend/src/target/fasm.rs
+++ backend/src/target/fasm.rs
@@ -1,14 +1,44 @@
 pub struct Fasm {}
 
-pub enum Instruction {
-    Mov { left: Operand, right: Operand },
+pub enum Instruction<'a> {
+    Mov {
+        left: Operand<'a>,
+        right: Operand<'a>,
+    },
+    Lea {
+        left: Operand<'a>,
+        right: Operand<'a>,
+    },
+    Syscall,
 }
 
-pub enum Operand {
-    Reg(),
-    Str(),
-    Int(),
-    Pointer(Box<Operand>),
+pub enum Operand<'a> {
+    Reg(Register),
+    Str(&'a str),
+    Int(usize),
+    Label(Label<'a>),
+    Pointer(Box<Operand<'a>>),
 }
 
-pub enum Register {}
+pub enum Register {
+    Rdx,
+    Rsi,
+    Rdi,
+    Rax,
+}
+
+pub enum Label<'a> {
+    DataDef {
+        data_dir: DataDirective,
+        name: &'a str,
+    },
+    CodeDef {
+        name: &'a str,
+    },
+}
+
+pub struct Segment {}
+
+pub enum DataDirective {
+    Db,
+}
