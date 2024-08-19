93a28eefd0d94153a3ff4a8ae63d294980409769
mrandr1an <krackedissad@gmail.com>
2024-08-19 20:19:35 +0300

    Deleted some files

--- backend/src/target/compiled.rs
+++
@@ -1,8 +0,0 @@
-use super::{assembler::assembly::Assembly, cached::Cached, saved::Saved};
-
-pub enum Written<'a, A: Assembly<'a>> {
-    Cached(Cached<'a, A>),
-    Saved(Saved<'a, A>),
-}
-
-pub struct Compiled {}
