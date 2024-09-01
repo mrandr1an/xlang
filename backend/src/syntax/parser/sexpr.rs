205444a7ac221e508bdcd14de643f72e65873d9f
mrandr1an <krackedissad@gmail.com>
2024-09-01 13:37:51 +0300

    Arena implementation of parser

--- backend/src/syntax/parser/sexpr.rs
+++ backend/src/syntax/parser/sexpr.rs
@@ -1,247 +1,241 @@
-use std::{
-    cell::{Ref, RefCell},
-    rc::{Rc, Weak},
+use std::{iter::Peekable, vec::IntoIter};
+
+use crate::syntax::tokenizer::{
+    token::{Lexeme, Token},
+    tokenized::{Tokenized, TokenizerError},
 };
 
-use crate::syntax::tokenizer::token::Lexeme;
-
 #[derive(Debug)]
-pub enum Sexpr<'a> {
-    Item(Item<'a>),
-    List(Rc<List<'a>>),
+pub struct List<'a> {
+    items: Vec<SexprType<'a>>,
+    parent: Option<ListPtr>,
+}
+
+impl<'a> List<'a> {
+    fn root() -> Self {
+        Self {
+            items: Vec::new(),
+            parent: None,
+        }
+    }
+
+    fn new(parent: ListPtr) -> Self {
+        Self {
+            items: Vec::new(),
+            parent: None,
+        }
+    }
+
+    fn iter(self) -> IntoIter<SexprType<'a>> {
+        self.items.into_iter()
+    }
+
+    fn get_parent(&self) -> Option<ListPtr> {
+        self.parent
+    }
 }
 
 #[derive(Debug)]
 pub struct Item<'a> {
-    lexeme: Lexeme<'a>,
-    parent: Weak<List<'a>>,
+    data: Lexeme<'a>,
+    parent: ListPtr,
+}
+
+impl<'a> Item<'a> {
+    fn new(lexeme: Lexeme<'a>, parent: ListPtr) -> Self {
+        Self {
+            data: lexeme,
+            parent,
+        }
+    }
+}
+
+type ListPtr = usize;
+
+#[derive(Debug)]
+pub enum SexprType<'a> {
+    List(ListPtr),
+    Item(Item<'a>),
 }
 
 #[derive(Debug)]
-pub struct List<'a> {
-    parent: Option<Weak<List<'a>>>,
-    elements: SexprElements<'a>,
+pub struct Arena<'a> {
+    lists: Vec<List<'a>>,
 }
 
-impl<'a> List<'a> {
-    pub fn root() -> Rc<Self> {
-        Rc::new(List {
-            parent: None,
-            elements: RefCell::new(vec![]),
-        })
+impl<'a> Arena<'a> {
+    fn new() -> Self {
+        Self { lists: Vec::new() }
     }
 
-    pub fn new(parent: Weak<List<'a>>) -> Rc<Self> {
-        Rc::new(List {
-            parent: Some(parent),
-            elements: RefCell::new(vec![]),
-        })
+    fn push(&mut self, list: List<'a>) -> ListPtr {
+        self.lists.push(list);
+        self.lists.len() - 1
     }
 
-    pub fn get_parent(&self) -> Option<Rc<List<'a>>> {
-        self.parent
-            .as_ref()
-            .map(|parent| parent.upgrade().expect("Weak pointer error"))
+    fn get(&self, id: ListPtr) -> Option<&List<'a>> {
+        self.lists.get(id)
     }
 
-    pub fn push_lexeme(self: &Rc<Self>, lexeme: Lexeme<'a>) {
-        self.elements.borrow_mut().push(Sexpr::Item(Item {
-            lexeme,
-            parent: Rc::downgrade(self),
-        }))
-    }
-
-    pub fn push_list(self: &Rc<Self>, list: Rc<List<'a>>) {
-        self.elements.borrow_mut().push(Sexpr::List(list))
-    }
-
-    pub fn push_sexpr(self: &Rc<Self>, sexpr: Sexpr<'a>) {
-        self.elements.borrow_mut().push(sexpr)
-    }
-
-    fn iter(self: &'a Rc<Self>) -> ListIter<'a> {
-        let borrowed_self = self.elements.borrow();
-        /* For future reference vector.as_slice() is == to &vector[..] latter is more elegant imo*/
-        ListIter {
-            inner: Some(Ref::map(borrowed_self, |inner_vector| &inner_vector[..])),
-        }
+    fn get_mut(&mut self, id: ListPtr) -> Option<&mut List<'a>> {
+        self.lists.get_mut(id)
     }
 }
 
-pub struct ListIter<'a> {
-    inner: Option<Ref<'a, [Sexpr<'a>]>>,
+#[derive(Debug)]
+pub struct Zipper<'a> {
+    mem: Arena<'a>,
 }
 
-impl<'a> Iterator for ListIter<'a> {
-    type Item = Ref<'a, Sexpr<'a>>;
-    fn next(&mut self) -> Option<Self::Item> {
-        match self.inner.take() {
-            Some(slice_ref) => match *slice_ref {
-                [] => None,
-                [_, ..] => {
-                    let (head_ref, tail_ref) =
-                        Ref::map_split(slice_ref, |slice| (&slice[0], &slice[1..]));
-                    self.inner.replace(tail_ref);
-                    Some(head_ref)
-                }
-            },
-            None => None,
-        }
+#[derive(Debug)]
+pub enum ZipperError<'a> {
+    TokenizerError(TokenizerError<'a>),
+    UnexpectedEOF,
+}
+
+impl<'a> From<TokenizerError<'a>> for ZipperError<'a> {
+    fn from(value: TokenizerError<'a>) -> Self {
+        ZipperError::TokenizerError(value)
     }
 }
 
-/*
-http://play.rust-lang.org/?version=stable&mode=debug&edition=2018&gist=517e738082574de914918476cb47c493
-https://stackoverflow.com/questions/33541492/returning-iterator-of-a-vec-in-a-refcell
- */
-
-type SexprElements<'a> = RefCell<Vec<Sexpr<'a>>>;
-
-impl<'a> Sexpr<'a> {
-    pub fn root() -> Self {
-        Self::List(List::root())
-    }
-
-    pub fn new(parent: &Sexpr<'a>) -> Self {
-        match parent {
-            Sexpr::List(parent_list) => Self::List(List::new(Rc::downgrade(parent_list))),
-            Sexpr::Item(_) => panic!("Item cannot be parent"),
+//Zipper creation from tokens
+impl<'a> Zipper<'a> {
+    fn new(tokens: &'a mut Tokenized<'a>) -> Option<Result<Self, Vec<ZipperError<'a>>>> {
+        let (tokens, errors): (Vec<_>, Vec<_>) = tokens.partition(Result::is_ok);
+        if errors.is_empty() {
+            let mut tokens = tokens
+                .into_iter()
+                .map(Result::unwrap)
+                .collect::<Vec<_>>()
+                .into_iter()
+                .peekable();
+            // let zippers = Vec::new();
+            match Self::next_root(&mut tokens) {
+                Some(Ok(zipper)) => Some(Ok(zipper)),
+                Some(Err(err)) => Some(Err(vec![err])),
+                None => None,
+            }
+        } else {
+            let errors = errors
+                .into_iter()
+                .map(|error| ZipperError::from(error.unwrap_err()))
+                .collect::<Vec<_>>();
+            Some(Err(errors))
         }
     }
 
-    pub fn add_lexeme(&self, lexeme: Lexeme<'a>) {
-        match self {
-            Self::List(list) => list.push_lexeme(lexeme),
-            Self::Item(_) => panic!("Cannot add lexeme to non list"),
-        }
-    }
-
-    pub fn add_list(&self, list: Rc<List<'a>>) {
-        match self {
-            Self::List(curr_list) => curr_list.push_list(list),
-            Self::Item(_) => panic!("Cannot add list to non list"),
-        }
-    }
-
-    pub fn add_sexpr(&self, sexpr: &Sexpr<'a>) {
-        match self {
-            Self::List(curr_list) => match sexpr {
-                Sexpr::Item(_) => todo!("Maybe this should not be implemented"),
-                Sexpr::List(list) => curr_list.push_list(Rc::clone(list)),
-            },
-            Self::Item(_) => panic!("Cannot add sexpr to non list"),
-        }
-    }
-
-    pub fn get_parent(&self) -> Option<Rc<List<'a>>> {
-        match self {
-            Sexpr::Item(item) => item.parent.upgrade(),
-            Sexpr::List(list) => {
-                if let Some(par) = &list.parent {
-                    par.upgrade()
-                } else {
-                    None
+    fn next_root(
+        tokens: &mut Peekable<IntoIter<Token<'a>>>,
+    ) -> Option<Result<Self, ZipperError<'a>>> {
+        match tokens.next() {
+            Some(Token::LParen(pos)) => {
+                let mut arena = Arena::new();
+                let root = List::root();
+                match Self::next_sexpr(tokens, arena.push(root), None, &mut arena) {
+                    Ok(_) => Some(Ok(Self { mem: arena })),
+                    Err(err) => Some(Err(err)),
                 }
             }
+            Some(Token::RParen(pos)) => todo!(), //expected lparen
+            Some(Token::Lexeme(lexeme)) => todo!(), //expected lparen
+            None => None,                        //expected lparen
         }
     }
 
-    pub fn iter(&'a self) -> SexprIter<'a> {
-        match self {
-            Sexpr::List(list) => SexprIter {
-                list_iter: list.iter(),
-            },
-            Sexpr::Item(_) => panic!("Cannot iterate an item"),
+    fn next_sexpr(
+        tokens: &mut Peekable<IntoIter<Token<'a>>>,
+        cur: ListPtr,
+        parent: Option<ListPtr>,
+        mem: &mut Arena<'a>,
+    ) -> Result<(), ZipperError<'a>> {
+        match tokens.next() {
+            Some(Token::LParen(pos)) => {
+                //Create new list that points to its parent in arena
+                let sexpr = List::new(cur);
+                Self::next_sexpr(tokens, mem.push(sexpr), Some(cur), mem)
+            }
+            Some(Token::RParen(pos)) => match parent {
+                Some(par_of_cur_ptr) => {
+                    let par_of_cur = mem
+                        .get_mut(par_of_cur_ptr)
+                        .expect("Parent not found (shouldnt happen)");
+                    par_of_cur.items.push(SexprType::List(cur));
+                    match par_of_cur.get_parent() {
+                        Some(par_of_par_ptr) => {
+                            Self::next_sexpr(tokens, par_of_cur_ptr, Some(par_of_par_ptr), mem)
+                        }
+                        None => Self::next_sexpr(tokens, par_of_cur_ptr, None, mem),
+                    }
+                }
+                None => Ok(()),
+            }, //expected lparen
+            Some(Token::Lexeme(lexeme)) => {
+                let cur_list = mem
+                    .get_mut(cur)
+                    .expect("Current not found (shouldnt hapen)");
+                cur_list.items.push(SexprType::Item(Item::new(lexeme, cur)));
+                Self::next_sexpr(tokens, cur, parent, mem)
+            } //expected lparen
+            None => Err(ZipperError::UnexpectedEOF), //expected lparen
+        }
+    }
+
+    pub fn iter(self) -> Sexpr<'a> {
+        Sexpr { mem: self.mem }
+    }
+}
+
+pub struct Sexpr<'a> {
+    mem: Arena<'a>,
+}
+
+impl<'a> Sexpr<'a> {
+    fn traverse_ref(&'a self) -> InorderTraverseRef<'a> {
+        InorderTraverseRef {
+            next: (0, 0),
+            cur_node: None,
+            mem_ref: &self.mem,
         }
     }
 }
 
-pub struct SexprIter<'a> {
-    list_iter: ListIter<'a>,
+pub struct InorderTraverseRef<'a> {
+    next: (ListPtr, usize),
+    cur_node: Option<&'a List<'a>>,
+    mem_ref: &'a Arena<'a>,
 }
 
-impl<'a> Iterator for SexprIter<'a> {
-    type Item = Ref<'a, Sexpr<'a>>;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.list_iter.next()
+impl<'a> Traverser for InorderTraverseRef<'a> {
+    type Item = &'a List<'a>;
+    fn step(&mut self) -> Option<Self::Item> {
+        let list = self.mem_ref.get(self.next.0);
+        self.next.0 += 1;
+        list
     }
+}
+
+pub trait Traverser {
+    type Item;
+    fn step(&mut self) -> Option<Self::Item>;
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::syntax::tokenizer::token::Lexeme;
-
-    use super::Sexpr;
-
-    #[test]
-    fn creating_sexprs() {
-        let root = Sexpr::root();
-
-        /* Pseudo lexemes */
-        let defun = Lexeme::Symbol {
-            range: 0..5,
-            literal: "defun",
-        };
-        let print = Lexeme::Symbol {
-            range: 0..5,
-            literal: "print",
-        };
-
-        root.add_lexeme(defun);
-        root.add_lexeme(print);
-
-        let args = Sexpr::new(&root);
-
-        let arg1 = Lexeme::Atom {
-            range: 10..15,
-            literal: "string",
-        };
-        let arg2 = Lexeme::Symbol {
-            range: 10..15,
-            literal: "msg",
-        };
-        args.add_lexeme(arg1);
-        args.add_lexeme(arg2);
-
-        root.add_sexpr(&args);
-
-        println!("{:#?}", root);
-    }
+    use crate::syntax::{
+        parser::sexpr::{Traverser, Zipper},
+        tokenizer::tokenized::Tokens,
+    };
 
     #[test]
-    fn iterating_sexprs() {
-        let root = Sexpr::root();
+    fn parse() {
+        let mut s = "(defun print ( :string msg ) \"Prints msg\" ( dbg msg ) ) ( exit 1 )".tokens();
+        let zipper = Zipper::new(&mut s).unwrap().unwrap();
+        let sexpr = zipper.iter();
+        let mut traverser = sexpr.traverse_ref();
 
-        /* Pseudo lexemes */
-        let defun = Lexeme::Symbol {
-            range: 0..5,
-            literal: "defun",
-        };
-        let print = Lexeme::Symbol {
-            range: 0..5,
-            literal: "print",
-        };
-
-        root.add_lexeme(defun);
-        root.add_lexeme(print);
-
-        let args = Sexpr::new(&root);
-
-        let arg1 = Lexeme::Atom {
-            range: 10..15,
-            literal: "string",
-        };
-        let arg2 = Lexeme::Symbol {
-            range: 10..15,
-            literal: "msg",
-        };
-        args.add_lexeme(arg1);
-        args.add_lexeme(arg2);
-
-        root.add_sexpr(&args);
-
-        for i in root.iter() {
-            println!("{:#?}", i)
-        }
+        println!("{:#?}", traverser.step());
+        println!("{:#?}", traverser.step());
+        println!("{:#?}", traverser.step());
     }
 }
